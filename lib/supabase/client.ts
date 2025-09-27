ff21e681e2fa7a428432a84b9dadac1c132a93f1
chriskbs <christiankevinbs@gmail.com>
2025-09-27 16:37:27 +0800

    updated env variables for vercel again

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,8 @@
 // lib/supabase/client.ts
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.SUPABASE_POSTGRES__NEXT_PUBLIC_SUPABASE_URL!;
-const anonKey = process.env.SUPABASE_POSTGRES__NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url = process.env.SUPABASE_POSTGRES_NEXT_PUBLIC_SUPABASE_URL!;
+const anonKey = process.env.SUPABASE_POSTGRES_NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 // Browser-side client (use anon key)
 export const supabase = createClient(url, anonKey);
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -9,8 +9,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.SUPABASE_POSTGRES__NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_POSTGRES__NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_POSTGRES_NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_POSTGRES_NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
